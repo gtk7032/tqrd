9f2d430fb48ff67d77fe6858e2fe1d995f2838ab
gtk7032 <gtk7032@gmail.com>
2022-09-28 23:05:32 +0900

    wip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import argparse
+import csv
 import os
 import re
-import sys
 from enum import Flag, auto
 from typing import Tuple
 
@@ -38,11 +39,11 @@ def extract_tables(query: str) -> Tuple[list[str], str]:
 
 
 def guess_query_type(query: str) -> QueryType:
-    if re.search(r"(DELETE)", query, flags=re.DOTALL | re.IGNORECASE) is not None:
+    if re.search(r"DELETE", query, flags=re.DOTALL | re.IGNORECASE) is not None:
         return QueryType.DELETE
-    elif re.search(r"(UPDATE)", query, flags=re.DOTALL | re.IGNORECASE) is not None:
+    elif re.search(r"UPDATE", query, flags=re.DOTALL | re.IGNORECASE) is not None:
         return QueryType.UPDATE
-    elif re.search(r"(INSERT)", query, flags=re.DOTALL | re.IGNORECASE) is not None:
+    elif re.search(r"INSERT", query, flags=re.DOTALL | re.IGNORECASE) is not None:
         return QueryType.INSERT
     else:
         return QueryType.SELECT
@@ -59,14 +60,29 @@ def select_color(query_type: QueryType) -> str:
         return "#e7298a"
 
 
+# def add_graph(path: str, dg: Digraph):
+
+#     with open(path, "r", encoding="utf-8") as f:
+#         rows = csv.reader(f)
+#         for row in rows:
+#             from_table = row[0]
+#             to_table = row[1]
+#             arrow_name = row[2]
+#             query_type = row[3]
+
+
 def main():
-    # files = ["file1.sql"]
-    sqlfiles = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(description="an example program")
+    parser.add_argument("--files", required=True, nargs="*", type=str)
+    parser.add_argument("--add", required=False, nargs=1, type=str)
+    parser.add_argument("--mappings", required=False, nargs=1, type=str)
+    args = parser.parse_args()
     dg = Digraph()
     dg.attr("graph", rankdir="LR")
 
-    for sqlfile in sqlfiles:
-        fnm = os.path.splitext(os.path.basename(sqlfile))[0]
+    for sqlfile in args.files:
+        procedure = os.path.splitext(os.path.basename(sqlfile))[0]
         with open(sqlfile, "r", encoding="utf-8") as f:
             queries = f.read().split(";")
             del queries[-1]  # empty
@@ -74,14 +90,22 @@ def main():
             for query in queries:
                 query_type = guess_query_type(query)
                 from_tables, to_table = extract_tables(query)
-                dg.node(to_table)
-
+                dg.node(
+                    to_table,
+                    shape="cylinder",
+                    color="white" if query_type == QueryType.SELECT else "black",
+                )
+                dg.node(procedure, shape="note")
+                dg.edge(
+                    tail_name=procedure,
+                    head_name=to_table,
+                    color=select_color(query_type),
+                )
                 for from_table in from_tables:
-                    dg.node(from_table)
+                    dg.node(from_table, shape="cylinder")
                     dg.edge(
                         tail_name=from_table,
-                        head_name=to_table,
-                        label=fnm,
+                        head_name=procedure,
                         color=select_color(query_type),
                     )
 
